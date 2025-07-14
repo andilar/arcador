e477293c3bf91c6c6600d4d6e010c91ee4ed243d
andilar <andilar@gmail.com>
2025-07-14 18:40:43 +0200

    Added Laser

--- game.py
+++ game.py
@@ -5,12 +5,18 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK)
         
-        # Position des Symbols
+        # Position des Raumschiffs
         self.player_x = 400
-        self.player_y = 300
+        self.player_y = 30
         
         # Bewegungsgeschwindigkeit
         self.speed = 5
+        
+        # Liste für Laser-Schüsse
+        self.lasers = []
+        
+        # Laser-Geschwindigkeit
+        self.laser_speed = 8
         
     def setup(self):
         pass
@@ -18,17 +24,27 @@ class MyGame(arcade.Window):
     def on_draw(self):
         self.clear()
         
-        # Weißes Dreieck als Symbol zeichnen
+        # Raumschiff (weißes Dreieck) zeichnen
         arcade.draw_triangle_filled(self.player_x, self.player_y + 15,  # Spitze oben
                                    self.player_x - 15, self.player_y - 15,  # Unten links
                                    self.player_x + 15, self.player_y - 15,  # Unten rechts
                                    arcade.color.WHITE)
         
+        # Laser-Schüsse zeichnen
+        for laser in self.lasers:
+            arcade.draw_circle_filled(laser['x'], laser['y'], 3, arcade.color.RED)
+        
     def on_update(self, delta_time):
-        pass
+        # Laser-Schüsse bewegen
+        for laser in self.lasers[:]:  # Kopie der Liste zum sicheren Entfernen
+            laser['y'] += self.laser_speed
+            
+            # Laser entfernen, wenn sie den Bildschirm verlassen
+            if laser['y'] > 600:
+                self.lasers.remove(laser)
         
     def on_key_press(self, key, modifiers):
-        # Pfeiltasten abfragen
+        # Pfeiltasten für Raumschiff-Bewegung
         if key == arcade.key.UP:
             self.player_y += self.speed
         elif key == arcade.key.DOWN:
@@ -37,9 +53,18 @@ class MyGame(arcade.Window):
             self.player_x -= self.speed
         elif key == arcade.key.RIGHT:
             self.player_x += self.speed
+        
+        # Leertaste für Laser-Schuss
+        elif key == arcade.key.SPACE:
+            # Neuen Laser an der Spitze des Raumschiffs erstellen
+            new_laser = {
+                'x': self.player_x,
+                'y': self.player_y + 15  # Startet an der Spitze des Dreiecks
+            }
+            self.lasers.append(new_laser)
 
 def main():
-    game = MyGame(800, 600, "Symbol bewegen")
+    game = MyGame(800, 600, "Raumschiff mit Laser")
     game.setup()
     arcade.run()
 
